8654a5a52f9cddac6cf8aef4d3134af30aa7456c
MuscleNrd <30961978+MuscleNrd@users.noreply.github.com>
2019-03-04 18:48:23 -0800

    Initial commit

---
+++ resources/views/inc/showcase.blade.php
@@ -0,0 +1,8 @@
+<div class="jumbotron text-center">
+    <div class="container" >
+        <h1> Welcome to VideoDivision.net</h1>
+        <p class="lead">
+            Say goodbye to Netflix , Hulu, and Crunchyroll!
+        </p>
+    </div>
+</div>
